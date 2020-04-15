21d5caf7115b4ee5d77766de82e093cb5011621f
emanuelbesliu <emanuelbesliuph@gmail.com>
2020-04-15 21:42:43 +0300

    Level0 finished, TODO bear dialogue

---
+++ Assets/Scripts/Animations.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    private Animator anim;
+    private MovementPlatformer move;
+    private Collision coll;
+    public SpriteRenderer sp;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        coll = GetComponentInParent<Collision>();
+        move = GetComponentInParent<MovementPlatformer>();
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("OnGround", coll.onGround);
+        anim.SetBool("WallGrab", move.wallGrab);
+        anim.SetBool("CanMove", move.canMove);
+    }
+
+
+    public void SetHorizontalMovement(float x, float y, float yVel)
+    {
+        anim.SetFloat(("HorizontalAxis"), Mathf.Abs(x));
+        anim.SetFloat(("VerticalAxis"), y);
+        anim.SetFloat(("VerticalVelocity"), yVel);
+    }
+
+    public void SetTrigger(string trigger)
+    {
+        anim.SetTrigger(trigger);
+    }
+
+    public void Flip(bool side)
+    {
+        if (move.wallGrab)
+        {
+            if (!side && sp.flipX)
+                return;
+            if (side && !sp.flipX)
+                return;
+
+        }
+
+        bool state = (side) ? false : true;
+        sp.flipX = state;
+    }
+}
---
+++ Assets/Scripts/ControllersLightController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllersLightController : MonoBehaviour
+{
+    public UnityEngine.Experimental.Rendering.Universal.Light2D[] stars;
+    private bool[] directions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        directions = new bool[21];
+        for(int i=0; i<stars.Length; i++)
+        {
+            directions[i] = true;
+        }
+
+        InvokeRepeating("UpdateStar", 0.11f, 0.11f);
+    }
+
+    bool updateLight(UnityEngine.Experimental.Rendering.Universal.Light2D star, bool direction, bool last)
+    {
+        if (star.intensity >= 1.5f)
+        {
+            direction = false;
+        }else if(star.intensity <= 0.28f && !last)
+        {
+            direction = true;
+        }else if(star.intensity <= 0.5f && last){
+            direction = true;
+        }
+
+        return direction;
+    }
+
+    void UpdateStar()
+    {
+        for(int i=0; i<stars.Length; i++){
+            directions[i] = updateLight(stars[i], directions[i], false);
+
+            if (directions[i])
+            {
+                stars[i].intensity += 0.0088f;
+            }
+            else
+            {
+                stars[i].intensity -= 0.0088f;
+            }
+         }
+    }
+}
