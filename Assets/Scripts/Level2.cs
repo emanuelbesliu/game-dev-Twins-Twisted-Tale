e8680ca0c74b4ce023f80b647585b58c24e44a44
emanuelbesliu <emanuelbesliuph@gmail.com>
2020-05-24 13:17:49 +0300

    Level2 ants dialogue + guardian

---
+++ Assets/Scripts/Level2.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2 : MonoBehaviour
+{
+    public GameObject green;
+
+    public GameObject guardianD;
+    public GameObject guardianText1;
+    public GameObject guardianText2;
+    public GameObject guardianText3;
+
+    public GameObject antsD;
+    public GameObject antsInfo;
+    public GameObject antsText1;
+    public GameObject antsText2;
+    public GameObject antsText3;
+
+    private bool firstAnts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GuardianSpeech());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if(green.transform.position.y >= 4.2 && !firstAnts){
+            firstAnts = true;
+            antsD.SetActive(true);
+        }
+
+        if(antsInfo.activeSelf && Input.GetButtonDown("Info")){
+            if(antsText1.activeSelf){
+                antsText1.SetActive(false);
+                antsText2.SetActive(true);
+            }else if(antsText2.activeSelf){
+                antsText2.SetActive(false);
+                antsText3.SetActive(true);
+                antsInfo.SetActive(false);
+            }
+        }
+    }
+
+
+    IEnumerator GuardianSpeech(){
+        guardianD.SetActive(true);
+        yield return new WaitForSeconds(15f);
+        guardianD.SetActive(false);
+        yield return new WaitForSeconds(25f);
+        guardianD.SetActive(true);
+        guardianText1.SetActive(false);
+        guardianText2.SetActive(true);
+        yield return new WaitForSeconds(20f);
+        guardianD.SetActive(false);
+        yield return new WaitForSeconds(30f);
+        guardianD.SetActive(true);
+        guardianText2.SetActive(false);
+        guardianText3.SetActive(true);
+    }
+}
